c131be2a44448615de36d3a76badc7701dd81b04
Jean Fabre <contact@fabrejean.net>
2016-11-15 11:54:28 +0300

    new actions start and stop playing

---
+++ Assets/uTomate Ecosystem Actions/Editor/Editor/StartPlaying.cs
@@ -0,0 +1,55 @@
+﻿//
+// Copyright (c) 2015 Ancient Light Studios
+// All Rights Reserved
+// 
+// http://www.ancientlightstudios.com
+//
+
+
+/*--- __ECO__ __UTOMATE__ __ACTION__ ---*/
+
+using System;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+
+namespace Ecosystem.Utomate
+{
+
+	[UTActionInfo(actionCategory = "Editor", sinceUTomateVersion="1.2.0")]
+	[UTDoc(title="Play Current Scene", description="Starts Playing the current Scene")]
+	[UTDefaultAction]
+	/// <summary>
+	/// Action that starts playing.
+	/// </summary>
+	public class StartPlaying : UTAction
+	{
+		
+		public override IEnumerator Execute (UTContext context)
+		{
+
+			try 
+			{
+				EditorApplication.SaveCurrentSceneIfUserWantsTo();
+
+				EditorApplication.isPlaying = true;
+			}
+			catch(Exception e) 
+			{
+				throw new UTFailBuildException("StartPlaying Scene failed. " + e.Message,this);
+			}
+			
+			yield return "";
+		}
+		
+		[MenuItem("Assets/Create/uTomate/Editor/StartPlaying")]
+		public static void AddAction ()
+		{
+			Create<StartPlaying> ();
+		}
+		
+	}
+
+}
---
+++ Assets/uTomate Ecosystem Actions/Editor/Editor/StopPlaying.cs
@@ -0,0 +1,53 @@
+﻿//
+// Copyright (c) 2015 Ancient Light Studios
+// All Rights Reserved
+// 
+// http://www.ancientlightstudios.com
+//
+
+
+/*--- __ECO__ __UTOMATE__ __ACTION__ ---*/
+
+using System;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+
+namespace Ecosystem.Utomate
+{
+
+	[UTActionInfo(actionCategory = "Editor", sinceUTomateVersion="1.2.0")]
+	[UTDoc(title="Stop Playing Scene", description="Stops Editor Playback")]
+	[UTDefaultAction]
+	/// <summary>
+	/// Action that stops playing.
+	/// </summary>
+	public class StopPlaying : UTAction
+	{
+		
+		public override IEnumerator Execute (UTContext context)
+		{
+
+			try 
+			{
+				EditorApplication.isPlaying = false;
+			}
+			catch(Exception e) 
+			{
+				throw new UTFailBuildException("StopPlaying failed. " + e.Message,this);
+			}
+			
+			yield return "";
+		}
+		
+		[MenuItem("Assets/Create/uTomate/Editor/Stop Playing")]
+		public static void AddAction ()
+		{
+			Create<StopPlaying> ();
+		}
+		
+	}
+
+}
